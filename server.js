fc6e82273d9afdebfb5a16907468510c0545f4bf
Larissa <larissadecastro18@gmail.com>
2022-07-19 17:11:26 -0300

    concluindo aula1, instalação do nodemon

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros':'Você chegou na página de livros',
+    '/autores':'Você está na página de autores',
+    '/editora': 'Lista de editoras',
+    '/sobre':'curiosidades'
+}
+
 const server = http.createServer((req, res)=>{
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Curso de Node");
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, ()=>{
