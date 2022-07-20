5e734fd5a0314e9419330171bdc8f8cf40b200dc
Larissa <larissadecastro18@gmail.com>
2022-07-20 15:10:47 -0300

    adicionando Schema do banco

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
-import db from "./config/dbConnect.js"
+import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js"
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", ()=>{
@@ -11,17 +12,20 @@ export const app = express();
 
 app.use(express.json())
 
-const livros = [
-    {id: 1, "titulo":"Senhor dos Aneis"},
-    {id:2, "titulo":"1984"}
-]
+// const livros = [
+//     {id: 1, "titulo":"Senhor dos Aneis"},
+//     {id:2, "titulo":"1984"}
+// ]
 
 app.get("/", (req, res)=>{
     res.status(200).send("Curso de Node")
 })
 
 app.get("/livros", (req, res)=>{
-    res.status(200).json(livros)
+    livros.find((err, livros)=>{
+     res.status(200).json(livros)
+    })
+    
 })
 
 app.get("/livros/:id",(req, res)=>{
