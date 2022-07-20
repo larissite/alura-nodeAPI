b495a92e31e60b3b88c219b418d54c9519df6f78
Larissa <larissadecastro18@gmail.com>
2022-07-20 14:59:40 -0300

    adicionando conexão com o mongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import db from "./config/dbConnect.js"
+
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", ()=>{
+    console.log("Banco conectado com sucesso!")
+})
 
 export const app = express();
+
 
 app.use(express.json())
 
